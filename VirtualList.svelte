e4603f478220f0e736ffc5c07dab2374e63a2739
Moein <tjex31@yandex.com>
2020-05-23 19:08:20 +0430

    feat: Add toTop, toBottom

--- VirtualList.svelte
+++ VirtualList.svelte
@@ -11,7 +11,9 @@
 	// read-only, but visible to consumers via bind:start
 	export let start = 0;
 	export let end = 0;
-	export const getViewport = () => viewport
+	export const getViewport = () => viewport;
+	export const toTop = () => viewport.scrollTop = 0;
+	export const toBottom = () => viewport.scrollTop = viewport_height;
 
 	// local state
 	let height_map = [];
